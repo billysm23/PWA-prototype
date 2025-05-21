b8a38bd1294b9247cb64e5eeebf80061646f01e8
billysm23 <bllysm23@gmail.com>
2025-05-21 08:55:08 +0700

    Update route.ts

--- app/api/bins/update/route.ts
+++ app/api/bins/update/route.ts
@@ -3,7 +3,7 @@ import { createServerClient } from '@supabase/ssr';
 import { cookies } from 'next/headers';
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function OPTIONS(req: NextRequest) {
+export async function OPTIONS() {
   return new NextResponse(null, {
     status: 204,
     headers: {
